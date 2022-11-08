ea678a2c7998252b8db86077a1f9256b233a4046
Hossein Pourghadiri <49945753+pourghadiriHossein@users.noreply.github.com>
2022-11-08 21:28:24 +0330

    Update myPostComponent.ts

--- src/components/dashboard/myPostComponent.ts
+++ src/components/dashboard/myPostComponent.ts
@@ -1,10 +1,10 @@
-const columns = [
+const columns: any = [
   { name: 'id', align: 'left', label: 'ID', field: 'id', sortable: true },
   { name: 'title', align: 'center', label: 'Title', field: 'title', sortable: true },
   { name: 'description', align: 'center', label: 'Description', field: 'description', sortable: true },
 ]
 
-const rows = [
+const rows: any = [
   {
     id: 1,
     title: 'Masal',
