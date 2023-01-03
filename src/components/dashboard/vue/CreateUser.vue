4fdccc5c4fd44a0ee9ec20b10c49f879d6f7eef1
Hossein Pourghadiri <49945753+pourghadiriHossein@users.noreply.github.com>
2023-01-03 18:50:28 +0330

    Update CreateUser.vue

--- src/components/dashboard/vue/CreateUser.vue
+++ src/components/dashboard/vue/CreateUser.vue
@@ -2,6 +2,7 @@
   import { defineProps, defineEmits, ref } from 'vue';
 
   const model = ref(null);
+  const choice = ref(null);
 
   const props = defineProps({
     modelValue: {
@@ -15,7 +16,10 @@
     },
     email: {
       default: ''
-    }
+    },
+    options: {
+      default: ['admin', 'user']
+    },
   });
 
   const emit = defineEmits(['update:model-value', 'accepted']);
@@ -56,6 +60,9 @@
             File Size
           </template>
         </q-file>
+      </q-card-section>
+      <q-card-section>
+        <q-select v-model="choice" :options="options" label="Role" />
       </q-card-section>
       <q-card-actions align="right" class="text-primary">
         <q-btn color="red" icon-right="close" label="Cancel" @click="close"/>
