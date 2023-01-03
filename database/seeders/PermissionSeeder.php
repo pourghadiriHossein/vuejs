5b0b237a9fba6b1131e159eefed7706ec87d3cca
Hossein Pourghadiri <49945753+pourghadiriHossein@users.noreply.github.com>
2023-01-03 17:00:03 +0330

    Update PermissionSeeder.php

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -20,20 +20,50 @@ class PermissionSeeder extends Seeder
         $admin = Role::create(['name' => Roles::ADMIN, 'guard_name' => 'api']);
         $user = Role::create(['name' => Roles::USER, 'guard_name' => 'api']);
 
-        Permission::create(['name' => Permissions::POST_CREATE, 'guard_name' => 'api']);
-        Permission::create(['name' => Permissions::POST_DELETE, 'guard_name' => 'api']);
-        Permission::create(['name' => Permissions::POST_UPDATE, 'guard_name' => 'api']);
-        Permission::create(['name' => Permissions::USER_DELETE, 'guard_name' => 'api']);
-        Permission::create(['name' => Permissions::USER_UPDATE, 'guard_name' => 'api']);
+        Permission::create(['name' => Permissions::VIEW_MY_PROFILE, 'guard_name' => 'api']);
+        Permission::create(['name' => Permissions::UPDATE_MY_ACCOUNT, 'guard_name' => 'api']);
 
-        $admin->givePermissionTo(Permissions::POST_CREATE);
-        $admin->givePermissionTo(Permissions::POST_DELETE);
-        $admin->givePermissionTo(Permissions::POST_UPDATE);
-        $admin->givePermissionTo(Permissions::USER_DELETE);
-        $admin->givePermissionTo(Permissions::USER_UPDATE);
+        Permission::create(['name' => Permissions::VIEW_ANY_POST, 'guard_name' => 'api']);
+        Permission::create(['name' => Permissions::LIKE_ANY_POST, 'guard_name' => 'api']);
 
-        $user->givePermissionTo(Permissions::POST_CREATE);
-        $user->givePermissionTo(Permissions::POST_DELETE);
-        $user->givePermissionTo(Permissions::POST_UPDATE);
+        Permission::create(['name' => Permissions::CREATE_NEW_POST, 'guard_name' => 'api']);
+        Permission::create(['name' => Permissions::UPDATE_MY_POST, 'guard_name' => 'api']);
+        Permission::create(['name' => Permissions::DELETE_MY_POST, 'guard_name' => 'api']);
+
+        Permission::create(['name' => Permissions::UPDATE_ANY_POST, 'guard_name' => 'api']);
+        Permission::create(['name' => Permissions::DELETE_ANY_POST, 'guard_name' => 'api']);
+
+        Permission::create(['name' => Permissions::VIEW_ANY_ACCOUNT, 'guard_name' => 'api']);
+        Permission::create(['name' => Permissions::CREATE_ANY_ACCOUNT, 'guard_name' => 'api']);
+        Permission::create(['name' => Permissions::UPDATE_ANY_ACCOUNT, 'guard_name' => 'api']);
+        Permission::create(['name' => Permissions::DELETE_ANY_ACCOUNT, 'guard_name' => 'api']);
+
+        $admin->givePermissionTo(Permissions::VIEW_MY_PROFILE);
+        $admin->givePermissionTo(Permissions::UPDATE_MY_ACCOUNT);
+
+        $admin->givePermissionTo(Permissions::VIEW_ANY_POST);
+        $admin->givePermissionTo(Permissions::LIKE_ANY_POST);
+
+        $admin->givePermissionTo(Permissions::CREATE_NEW_POST);
+        $admin->givePermissionTo(Permissions::UPDATE_MY_POST);
+        $admin->givePermissionTo(Permissions::DELETE_MY_POST);
+
+        $admin->givePermissionTo(Permissions::UPDATE_ANY_POST);
+        $admin->givePermissionTo(Permissions::DELETE_ANY_POST);
+
+        $admin->givePermissionTo(Permissions::VIEW_ANY_ACCOUNT);
+        $admin->givePermissionTo(Permissions::CREATE_ANY_ACCOUNT);
+        $admin->givePermissionTo(Permissions::UPDATE_ANY_ACCOUNT);
+        $admin->givePermissionTo(Permissions::DELETE_ANY_ACCOUNT);
+
+        $user->givePermissionTo(Permissions::VIEW_MY_PROFILE);
+        $user->givePermissionTo(Permissions::UPDATE_MY_ACCOUNT);
+
+        $user->givePermissionTo(Permissions::VIEW_ANY_POST);
+        $user->givePermissionTo(Permissions::LIKE_ANY_POST);
+
+        $user->givePermissionTo(Permissions::CREATE_NEW_POST);
+        $user->givePermissionTo(Permissions::UPDATE_MY_POST);
+        $user->givePermissionTo(Permissions::DELETE_MY_POST);
     }
 }
